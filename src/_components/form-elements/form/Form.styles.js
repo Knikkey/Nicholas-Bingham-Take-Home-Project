6b878a88e0983ecf798453e3f678d4ad406891ce
Knikkey <102648803+Knikkey@users.noreply.github.com>
2023-10-26 16:49:21 -0700

    feature: implemented form component with styles

---
+++ src/_components/form-elements/form/Form.styles.js
@@ -0,0 +1,12 @@
+"use client";
+import styled from "styled-components";
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  padding: 0.5rem 1rem;
+  border: 1px solid black;
+  border-radius: 7px;
+  box-shadow: 0px 1px 1px #00000047;
+`;

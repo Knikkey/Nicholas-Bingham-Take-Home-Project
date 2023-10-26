76597efe88fea8672899cbe18865e99d148845c1
Knikkey <102648803+Knikkey@users.noreply.github.com>
2023-10-26 16:07:18 -0700

    feature: implemented select component with styles

---
+++ src/_components/form-elements/select/Select.styles.js
@@ -0,0 +1,11 @@
+"use client";
+import styled from "styled-components";
+
+export const Select = styled.select`
+  width: fit-content;
+  padding: 0.5rem 1rem;
+  background-color: transparent;
+  border: 1px solid black;
+  border-radius: 7px;
+  transition: all 0.3s;
+`;

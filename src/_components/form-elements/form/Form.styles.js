6a8426c5f14f2cc24191ad6700a4886dac436056
Knikkey <102648803+Knikkey@users.noreply.github.com>
2023-10-26 23:14:06 -0700

    style: added text alignment

--- src/_components/form-elements/form/Form.styles.js
+++ src/_components/form-elements/form/Form.styles.js
@@ -6,6 +6,7 @@ export const Form = styled.form`
   flex-direction: column;
   gap: 1rem;
   padding: 0.5rem 1rem;
+  text-align: center;
   border: 1px solid black;
   border-radius: 7px;
   box-shadow: 0px 1px 1px #00000047;

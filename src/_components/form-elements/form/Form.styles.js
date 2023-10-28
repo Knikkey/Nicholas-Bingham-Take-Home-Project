e4049031ecfc720aec6b75006b8164d725d13dcc
Knikkey <102648803+Knikkey@users.noreply.github.com>
2023-10-28 10:36:52 -0700

    feature: fixed responsiveness

--- src/_components/form-elements/form/Form.styles.js
+++ src/_components/form-elements/form/Form.styles.js
@@ -10,5 +10,14 @@ export const Form = styled.form`
   width: 40%;
   border: 1px solid #c7c7c7;
   border-radius: 20px;
-  box-shadow: 10px 5px 10px black;
+  box-shadow: 5px 2px 10px #333;
+
+  @media (max-width: 1100px) {
+    width: 65%;
+  }
+
+  @media (max-width: 700px) {
+    width: 100%;
+    margin: 0 1rem;
+  }
 `;

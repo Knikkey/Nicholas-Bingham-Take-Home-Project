fa68c7454b44fd37e37d844acced0ef50275717a
Knikkey <102648803+Knikkey@users.noreply.github.com>
2023-10-27 18:15:40 -0700

    chore: removed empty dependancy from useEffect

--- src/app/confirmation-page/page.tsx
+++ src/app/confirmation-page/page.tsx
@@ -5,62 +5,75 @@ import { Input } from "@/_components/form-elements/input/Input.styles";
 import { Select } from "@/_components/form-elements/select/Select.styles";
 import { FlexDiv } from "@/_components/divs/flex-div/FlexDiv.styles";
 import { formFields } from "@/data/data.js";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 const noDataMessage = "Data not provided";
 
 export default function Page() {
   const { data } = useTypedSelector((state) => state.jobAppForm);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!data) router.push("/");
+  });
 
   return (
-    <Form as="div">
-      <h1>Application Submitted</h1>
-      <p>Below is the submitted data</p>
-      {data &&
-        formFields.map((field) => {
-          if (Array.isArray(field)) {
-            return (
-              <FlexDiv key={Math.random()}>
-                {field.map((item) => (
+    <>
+      {data && (
+        <Form as="div">
+          <h1>Application Submitted</h1>
+          <p>Below is the submitted data</p>
+          {data &&
+            formFields.map((field) => {
+              if (Array.isArray(field)) {
+                return (
+                  <FlexDiv key={Math.random()}>
+                    {field.map((item) => (
+                      <Input
+                        value={data[item.id] || noDataMessage}
+                        key={item.id}
+                        disabled
+                        aria-label={item.placeholder}
+                      />
+                    ))}
+                  </FlexDiv>
+                );
+              }
+              if (field.type === "textarea") {
+                return (
                   <Input
-                    value={data[item.id] || noDataMessage}
-                    key={item.id}
+                    key={field.id}
+                    as="textarea"
+                    value={data[field.id] || noDataMessage}
                     disabled
-                    aria-label={item.placeholder}
+                    aria-label={field.placeholder}
                   />
-                ))}
-              </FlexDiv>
-            );
-          }
-          if (field.type === "textarea") {
-            return (
-              <Input
-                key={field.id}
-                as="textarea"
-                value={data[field.id] || noDataMessage}
-                disabled
-                aria-label={field.placeholder}
-              />
-            );
-          }
-          if (field.type === "select") {
-            return (
-              <FlexDiv key={field.id} $row>
-                <label htmlFor={field.id}>Select a role to apply for:</label>
-                <Select id={field.id} disabled>
-                  <option>{data[field.id] || noDataMessage}</option>
-                </Select>
-              </FlexDiv>
-            );
-          } else
-            return (
-              <Input
-                key={field.id}
-                value={data[field.id] || noDataMessage}
-                disabled
-                aria-label={field.placeholder}
-              />
-            );
-        })}
-    </Form>
+                );
+              }
+              if (field.type === "select") {
+                return (
+                  <FlexDiv key={field.id} $row>
+                    <label htmlFor={field.id}>
+                      Select a role to apply for:
+                    </label>
+                    <Select id={field.id} disabled>
+                      <option>{data[field.id] || noDataMessage}</option>
+                    </Select>
+                  </FlexDiv>
+                );
+              } else
+                return (
+                  <Input
+                    key={field.id}
+                    value={data[field.id] || noDataMessage}
+                    disabled
+                    aria-label={field.placeholder}
+                  />
+                );
+            })}
+        </Form>
+      )}
+    </>
   );
 }

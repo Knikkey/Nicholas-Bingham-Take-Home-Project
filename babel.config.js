08a72a3d51a7591654879b176f50723c9ef51b58
Knikkey <102648803+Knikkey@users.noreply.github.com>
2023-10-27 21:26:01 -0700

    feature: added test coverage tracking

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,16 @@
 module.exports = {
   presets: [
-    ["@babel/preset-env", { targets: { node: "current" } }],
-    "@babel/preset-typescript",
+    "@babel/preset-env",
+    ["@babel/preset-react", { runtime: "automatic" }],
+  ],
+  plugins: [
+    [
+      "styled-components",
+      {
+        ssr: true,
+        displayName: true,
+        preprocess: false,
+      },
+    ],
   ],
 };
--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,16 @@
 module.exports = {
+  testPathIgnorePatterns: [
+    "<rootDir>/.next/",
+    "<rootDir>/node_modules/",
+    "<rootDir>/src/",
+  ],
   testEnvironment: "jsdom",
+  transform: {
+    "^.+.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
+  },
+  transformIgnorePatterns: ["/node_modules/", "^.+.module.(css|sass|scss)$"],
+  moduleNameMapper: {
+    "^.+.module.(css|sass|scss)$": "identity-obj-proxy",
+  },
+  collectCoverageFrom: ["src/**/*.{js,jsx,ts,tsx}", "!src/**/*.d.ts"],
 };

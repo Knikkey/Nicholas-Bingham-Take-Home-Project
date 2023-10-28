0295b5af3395e43e9f83978feab8d4796556c162
Knikkey <102648803+Knikkey@users.noreply.github.com>
2023-10-28 10:03:06 -0700

    style: added background img to main and more styling to form

--- src/_components/form-elements/form/Form.styles.js
+++ src/_components/form-elements/form/Form.styles.js
@@ -6,7 +6,9 @@ export const Form = styled.form`
   gap: 1rem;
   padding: 0.5rem 1rem;
   text-align: center;
-  border: 1px solid black;
-  border-radius: 7px;
-  box-shadow: 0px 1px 1px #00000047;
+  background-color: white;
+  width: 40%;
+  border: 1px solid #c7c7c7;
+  border-radius: 20px;
+  box-shadow: 10px 5px 10px black;
 `;
